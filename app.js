3bece716ba02425ae23e23adb91d892de67552d6
Wajid Usto <wajidusto@gmail.com>
2024-11-19 02:14:40 +0500

    command added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 let cursorMain = document.querySelector("#cursor")
 let mainWrapper = document.querySelector("#main")
+let page1 = document.querySelector("#page-1")
 
 
 // Cursor
@@ -13,4 +14,24 @@ mainWrapper.addEventListener("mousemove", (dets) => {
         duration: 3.5,
         ease: "elastic.out(1,0.3)",
     })
+})
+// Cursor
+// Cursor
+
+
+// Gsap ScrollTrigger 
+// Page1
+
+// let tl = gsap.timeline()
+
+gsap.to("#page-1",{
+    scrollTrigger:{
+        trigger:"#page-1",
+        scroller:"body",
+        markers:true,
+        start:"top 0%",
+        end:"top -100%",
+        // scrub:true,
+        // pin:true,
+    }
 })
