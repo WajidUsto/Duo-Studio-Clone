301acd05fae0dfea2f4e1b1743623841d60038b7
Wajid Usto <wajidusto@gmail.com>
2024-11-19 16:14:28 +0500

    command added

--- app.js
+++ app.js
@@ -6,10 +6,39 @@ let page1 = document.querySelector("#page-1")
 
 
 // Locomotive Scroll
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
+function Locomotive(params) {
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
     smooth: true
 });
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+Locomotive()
 // Locomotive Scroll
 
 
@@ -31,16 +60,26 @@ mainWrapper.addEventListener("mousemove", (dets) => {
 // Gsap ScrollTrigger 
 // Page1
 
-// let tl = gsap.timeline()
-
-gsap.to("#page-1",{
-    scrollTrigger:{
-        trigger:"#page-1",
-        scroller:"body",
-        markers:true,
-        start:"top 0%",
-        end:"top -100%",
-        // scrub:true,
-        // pin:true,
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#page-1",
+        scroller: "#main",
+        markers: true,
+        start: "35% 35%",
+        end: "60% 20%",
+        scrub : true,
+        pin:true,
     }
 })
+
+tl.to("#head-1", {
+    x : -100,
+    filter : "blur(5px)"
+} , "head")
+tl.to("#head-2", {
+    x : 100,
+    filter : "blur(5px)"
+} , "head")
+tl.to("#page-1-para p",{
+    filter : "blur(5px)"
+},"head")
